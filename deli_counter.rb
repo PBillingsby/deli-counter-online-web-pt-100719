b7aaaaee5c20dccbb89eef37f0145e376e477916
Phil Billingsby <paulbillingsby@gmail.com>
2020-01-19 14:04:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,9 +6,17 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else
+<<<<<<< HEAD
     line_msg = "The line is currently:"
     array.each_with_index {|value, index| line_msg += " #{index.to_i + 1}. #{value}"}
     puts "#{line_msg}"
+=======
+    message = "The line is currently:" 
+  array.each_with_index do |value, index|
+    message += " #{index.to_i + 1}. #{value}" 
+  end
+    puts "#{message}"
+>>>>>>> 52225892d7004f4c1d6466cb94df3a393ef66c4e
   end
 end
 
@@ -21,8 +29,14 @@ def now_serving(people)
   if people.count == 0
     puts "There is nobody waiting to be served!"
   else
+<<<<<<< HEAD
     name = people[0]
     puts "Currently serving #{name}."
     people.shift
+=======
+    name = n[0] # first name in line
+    puts "Currently serving #{name}." 
+    n.shift
+>>>>>>> 52225892d7004f4c1d6466cb94df3a393ef66c4e
   end
 end
