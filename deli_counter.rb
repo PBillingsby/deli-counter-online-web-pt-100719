418d1027947bb57ddcc85d8c622c467355e79968
Phil Billingsby <paulbillingsby@gmail.com>
2020-01-19 14:10:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,7 @@
 katz_deli = []
 def line(arr)
-  case arr.count == 0
+  if arr.count == 0
     "There is nobody in the line"
+  else
+    str = "The current line is "
+    arr.each_with_index do |person|
