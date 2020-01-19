e6a60fbdaa7fd7489bc4cc1ea18b1963a48d98b1
Phil Billingsby <paulbillingsby@gmail.com>
2020-01-19 14:14:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(arr)
   if arr.length == 0
     "The line is currently empty."
   else
-    str = "The current line is "
-    arr.each_with_index do |person|
-      str << person
+    str = "The current line is:"
+    arr.each_with_index do |index, person|
+      str << "#{index.to_i}. #{person}"
     end
   end
 end
